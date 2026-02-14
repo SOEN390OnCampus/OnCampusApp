e6fe8e8964288b559cbb416d8f0ea4de4a7f9eb7
Curtis Moxebo <cmoxebo@gmail.com>
2026-02-13 22:32:06 -0500

    Fix current location lag

--- app/src/main/java/com/example/oncampusapp/MapsActivity.java
+++ app/src/main/java/com/example/oncampusapp/MapsActivity.java
@@ -16,16 +16,15 @@ import android.util.Log;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.oncampusapp.databinding.ActivityMapsBinding;
-import com.google.android.gms.maps.model.PolygonOptions;
 import com.google.maps.android.data.Geometry;
 import com.google.maps.android.data.geojson.GeoJsonFeature;
 import com.google.maps.android.data.geojson.GeoJsonLayer;
@@ -49,8 +48,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     public static final LatLng SGW_COORDS = new LatLng(45.496107243097704, -73.57725834380621);
     public static final LatLng LOY_COORDS = new LatLng(45.4582, -73.6405);
+    public FusedLocationProviderClient fusedLocationClient;
 
     private ActivityResultLauncher<String[]> locationPermissionRequest;
+    private TextView btnSgwLoy;
 
     public GoogleMap getMap() {
         return this.mMap;
@@ -79,6 +80,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(binding.getRoot());
 
         buildingClassifier = new BuildingClassifier();
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -220,43 +223,55 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         .build()
         ));
 
-        TextView btnSgwLoy = findViewById(R.id.btn_campus_switch);
+        btnSgwLoy = findViewById(R.id.btn_campus_switch);
         ImageButton btnLocation = findViewById(R.id.btn_location);
 
-        // Click listener to switch between SGW and loyola campus
-        btnSgwLoy.setOnClickListener(v -> {
-            String currentText = btnSgwLoy.getText().toString();
-            String sgw = getResources().getString(R.string.campus_sgw);
-            String loy = getResources().getString(R.string.campus_loy);
+        btnSgwLoy.setOnClickListener(v -> switchCampus());
+        btnLocation.setOnClickListener(v -> goToCurrentLocation());
+    }
 
-            if (currentText.equals("SGW")) {
-                btnSgwLoy.setText(loy);
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(SGW_COORDS, 16f));
-            } else {
-                btnSgwLoy.setText(sgw);
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(LOY_COORDS, 16f));
-            }
-        });
+    // Switch between SGW and Loyola campus on the map
+    private void switchCampus() {
+        String currentText = btnSgwLoy.getText().toString();
+        String sgw = "SGW";
+        String loy = "LOY";
 
-        // Click listener to navigate to the current location
-        btnLocation.setOnClickListener(v -> {
-            // Check Permissions
-            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // Request Permissions if not granted
-                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-            } else {
-                // If granted, enable location and move camera
-                mMap.setMyLocationEnabled(true);
+        if (currentText.equals(sgw)) {
+            btnSgwLoy.setText(loy);
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(SGW_COORDS, 16f));
+        } else {
+            btnSgwLoy.setText(sgw);
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(LOY_COORDS, 16f));
+        }
+    }
 
-                // Get the current location from the map's internal "My Location" data
-                android.location.Location myLocation = mMap.getMyLocation();
+    // Set the map view to the current location
+    private void goToCurrentLocation() {
+        // Check Permissions
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Request Permissions if not granted
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        } else {
+            // If granted, enable location and move camera
+            mMap.setMyLocationEnabled(true);
 
-                if (myLocation != null) {
-                    LatLng myLatLng = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
-                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myLatLng, 15f));
-                }
-            }
-        });
+            // Get the current location from the device and set it on the map
+            fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, location -> {
+                    // Check if the location is not null
+                    if (location != null) {
+                        // Get the Latitude (as a double)
+                        double lat = location.getLatitude();
+                        double lng = location.getLongitude();
+
+                        // Use it! (e.g., move the camera)
+                        LatLng currentLatLng = new LatLng(lat, lng);
+                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 16f));
+
+                        Log.d("Location", "Current Latitude: " + lat);
+                    }
+                });
+        }
     }
 
     private void createNotificationChannel() {
