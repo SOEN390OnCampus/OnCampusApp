019372e138ea7ba2345bd2d15b652c139ac240aa
Nesrine <nesrine.larbi@mail.concordia.ca>
2026-03-30 21:58:59 -0400

    refactor: Add missing import for Log in EventBannerManager

--- app/src/main/java/com/example/oncampusapp/EventBannerManager.java
+++ app/src/main/java/com/example/oncampusapp/EventBannerManager.java
@@ -3,6 +3,7 @@ package com.example.oncampusapp;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
